9a1dcef3c6f88b0061cb32544a5e6f3ff5a614cb
Jacob Meyers <jacobmeyers6@outlook.com>
2023-03-09 09:40:50 -0800

    fixed animations

--- develop/src/App.js
+++ develop/src/App.js
@@ -6,8 +6,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <Header />
-        <PageContainer />
       </header>
+      <PageContainer />
     </div>
   );
 }
