4f4f26916c9858fb52213987d57ffc5730462a0d
Jacob Meyers <jacobmeyers6@outlook.com>
2023-03-30 11:03:08 -0700

    MVP

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,5 +1,13 @@
 import React from 'react';
+import '../../styles/Resume.css';
 
 export default function Resume() {
-   
+
+  return (
+    <div className='resume-container'>
+<a href='/downloadables/jacob-meyers-resume.docx' download='Jacob Meyers Resume' target='_blank'>
+  <button>Download résumé</button>
+</a>
+    </div>
+  )
 }
